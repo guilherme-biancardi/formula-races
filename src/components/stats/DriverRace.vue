11ac58f2bb91ee8be5b7c66dd89f734a92b461d8
biancardi_ <guilhermerbiancardi@gmail.com>
2022-07-29 16:41:30 -0300

    update compare-drivers feature

--- src/components/stats/DriverRace.vue
+++ src/components/stats/DriverRace.vue
@@ -1,30 +1,60 @@
 <template>
-  <ul class="race-list">
-    <li v-for="(item, index) in driverRace" :key="index">
-      <div class="item-text" v-if="item.value">
-        <p>{{ item.label }}</p>
-        <h3>{{ item.value }}</h3>
-      </div>
-      <a :href="item.link" v-if="!item.value" target="_blank">{{
+  <div class="race-content">
+    <ul class="race-list">
+      <li v-for="(item, index) in driverRace" :key="index">
+        <div class="item-text" v-if="item.value">
+          <p>{{ item.label }}</p>
+          <h3>{{ item.value }}</h3>
+        </div>
+        <a :href="item.link" v-if="!item.value" target="_blank">{{
           item.label
-      }}</a>
-      <MaterialIcon :icon="item.icon"></MaterialIcon>
-    </li>
-  </ul>
+        }}</a>
+        <MaterialIcon :icon="item.icon"></MaterialIcon>
+      </li>
+    </ul>
+    <button class="btn-drivers" @click="changeDriver" v-if="compareDrivers">
+      <MaterialIcon
+        :icon="`mdi-chevron-${state.index ? 'right' : 'left'}`"
+      ></MaterialIcon>
+    </button>
+  </div>
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, onMounted, reactive, watch } from 'vue'
 import MaterialIcon from '../utilities/MaterialIcon.vue'
 
 const props = defineProps({
   result: Object,
-  race: Object
+  race: Object,
+  compareDrivers: Boolean,
+  index: Number,
+  raceSelected: Number,
+  drivers: Array
 })
+
+const state = reactive({
+  index: false
+})
+
+const emit = defineEmits(['setDriverName'])
+
+const changeDriver = () => {
+  state.index = !state.index
+  emit('setDriverName', props.drivers[Number(state.index)])
+}
+
+watch(
+  computed(() => props.index),
+  (index) => (state.index = index)
+)
+
+onMounted(() => (state.index = props.index))
 
 const driverRace = computed(() => {
   try {
-    const { grid, FastestLap, positionText, status } = props.result
+    const { grid, FastestLap, positionText, status } =
+      props.result[Number(state.index)][props.raceSelected]
     const { url, raceName } = props.race
 
     return [
@@ -61,15 +91,24 @@ const driverRace = computed(() => {
 </script>
 
 <style scoped>
-.race-list {
+.race-content {
   --item: #fff;
   --text: #555;
 }
 
-.dark-mode .race-list {
+.dark-mode .race-content {
   --item: #252525;
   --text: #999;
 }
+
+.race-content {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
 .race-list {
   width: 100%;
   height: 100%;
@@ -105,7 +144,7 @@ const driverRace = computed(() => {
   font-weight: 100;
 }
 
-i {
+.race-list i {
   font-size: 1.5em;
   color: var(--red);
 }
@@ -114,5 +153,20 @@ a {
   font-size: 0.7em;
   text-decoration: underline;
   color: var(--text);
+}
+
+.btn-drivers {
+  position: absolute;
+  left: calc(100% + 20px);
+  background-color: var(--item);
+  padding: 8px 10px;
+  border-radius: 8px;
+  box-shadow: var(--shadow);
+  cursor: pointer;
+}
+
+.btn-drivers i {
+  font-size: 1.7em;
+  color: var(--red);
 }
 </style>
--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -1,14 +1,46 @@
 <template>
   <div class="stats-select">
-    <SelectComponent :label="'select driver:'" @select="selectDriver">
-      <option
-        :value="index"
-        v-for="({ Driver }, index) in drivers"
-        :key="index"
+    <div class="driver-selects">
+      <SelectComponent
+        :label="'select driver:'"
+        @select="selectDriver"
+        @click="state.selectIndex = 0"
       >
-        {{ Driver.givenName }} {{ Driver.familyName }}
-      </option>
-    </SelectComponent>
+        <option
+          :value="index"
+          v-for="({ Driver }, index) in drivers"
+          :key="index"
+          :disabled="state.driversListExcludes.some((i) => i === index)"
+        >
+          {{ Driver.givenName }} {{ Driver.familyName }}
+        </option>
+      </SelectComponent>
+      <button
+        v-if="!state.compareDrivers"
+        @click="state.compareDrivers = !state.compareDrivers"
+      >
+        <MaterialIcon :icon="'mdi-plus'"></MaterialIcon>
+      </button>
+      <SelectComponent
+        :label="'select driver:'"
+        @select="selectDriver"
+        @click="state.selectIndex = 1"
+        v-else
+      >
+        <option
+          :value="index"
+          v-for="({ Driver }, index) in drivers"
+          :disabled="state.driversListExcludes.some((i) => i === index)"
+          :key="index"
+        >
+          {{ Driver.givenName }} {{ Driver.familyName }}
+        </option>
+      </SelectComponent>
+      <button v-if="state.compareDrivers" @click="reset">
+        <MaterialIcon :icon="'mdi-delete-outline'"></MaterialIcon>
+      </button>
+    </div>
+
     <SelectComponent
       :label="`select ${state.driverName} race:`"
       @select="selectRace"
@@ -24,8 +56,13 @@
       :isLoading="state.loading"
     ></GraphicComponent>
     <DriverRace
-      :result="state.results[state.raceSelected]"
+      :result="state.results"
       :race="races[state.raceSelected]"
+      :compareDrivers="state.compareDrivers"
+      :raceSelected="state.raceSelected"
+      :index="state.selectIndex"
+      :drivers="state.driversListExcludes"
+      @setDriverName="setDriverName"
     ></DriverRace>
   </div>
 </template>
@@ -41,6 +78,7 @@ import SelectComponent from '@/components/utilities/SelectComponent.vue'
 import { useDriversStore } from '@/store/drivers'
 import { useRacesStore } from '@/store/races'
 import { useStatsStore } from '@/store/stats'
+import MaterialIcon from '@/components/utilities/MaterialIcon.vue'
 
 const store = useStore()
 const statsStore = useStatsStore()
@@ -52,27 +90,52 @@ const drivers = computed(() => useDriversStore().getStanding)
 const races = computed(() => useRacesStore().getTable.Races)
 
 const state = reactive({
-  graphic: null,
+  graphic: [],
   results: [],
   raceSelected: 0,
   driverName: '',
-  loading: true
+  loading: true,
+  compareDrivers: false,
+  selectIndex: 0,
+  driversListExcludes: []
 })
 
 const selectRace = (index) => (state.raceSelected = index)
+
+const reset = () => {
+  state.graphic = state.graphic.slice(0, 1)
+  state.results = state.results.slice(0, 1)
+  state.driversListExcludes = state.driversListExcludes.slice(0, 1)
+
+  state.selectIndex = 0
+  state.driverName =
+    drivers.value[state.driversListExcludes[0]].Driver.familyName
+  state.compareDrivers = false
+}
+
+const setDriverName = (index) =>
+  (state.driverName = drivers.value[index].Driver.familyName)
 
 const selectDriver = async (index) => {
   state.loading = true
 
   const { Driver } = drivers.value[index]
-  const { driverId, familyName } = Driver
+  const { driverId, givenName, familyName } = Driver
   const { graphic, results } =
-    statsStore.getDriverStat(driverId) ||
+    (await statsStore.getDriverStat(driverId)) ||
     (await getDriverResults(axios, season.value, driverId))
 
-  state.graphic = graphic
-  state.results = results
+  state.graphic[state.selectIndex] = {
+    name: `${givenName} ${familyName}`,
+    data: graphic
+  }
+
+  state.results[state.selectIndex] = results
+
   state.driverName = familyName
+  state.driverSelected = state.selectIndex
+
+  state.driversListExcludes[state.selectIndex] = index
 
   statsStore.setDriverStat(graphic, results, driverId)
   state.loading = false
@@ -87,6 +150,14 @@ watch(drivers, () => {
 </script>
 
 <style scoped>
+.stats-select {
+  --item: #fff;
+}
+
+.dark-mode .stats-select {
+  --item: #252525;
+}
+
 .stats-select,
 .stats-content {
   width: 100%;
@@ -97,5 +168,22 @@ watch(drivers, () => {
 
 .stats-content {
   min-height: 400px;
+}
+
+.driver-selects {
+  display: flex;
+  align-items: flex-end;
+  column-gap: 12px;
+}
+
+.driver-selects button {
+  width: 32px;
+  height: 32px;
+  border-radius: 5px;
+  background-color: var(--item);
+  box-shadow: var(--shadow);
+  cursor: pointer;
+  font-size: 1.05em;
+  color: var(--red);
 }
 </style>
