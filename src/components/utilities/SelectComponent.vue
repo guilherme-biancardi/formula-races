f435a48496b97fd4183a5e7de4a34c483b3044a1
guilherme-biancardi <guilhermerbiancardi@outlook.com>
2023-04-09 21:18:19 -0300

    criação das view equipes/corridas

---
+++ src/components/utilities/SelectComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <select @change="emit('select', Number($event.target.value))">
+    <option :value="null" v-if="defaultValue" disabled>
+      {{ defaultValue }}
+    </option>
+    <option :value="index" v-for="(value, index) in values" :key="index">
+      {{ value }}
+    </option>
+  </select>
+</template>
+
+<script setup>
+const emit = defineEmits(["select"]);
+
+defineProps({
+  defaultValue: String,
+  values: Array,
+});
+</script>
+
+<style scoped>
+.light select {
+  --background: #fff;
+  --border: #ddd;
+}
+
+select {
+  all: unset;
+  min-width: 24ch;
+  height: fit-content;
+  padding: 10px 12px;
+  padding-right: 32px;
+  background-color: var(--background);
+  border: 1px solid var(--border);
+  background-image: url("../../assets/image/chevron-down.svg");
+  background-repeat: no-repeat;
+  background-position: calc(100% - 6px);
+  background-size: 26px;
+  border-radius: 8px;
+  font-size: 0.8em;
+}
+</style>
--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,13 +1,56 @@
 <template>
-    <div>
-        <h1>equipes</h1>
-    </div>
+  <TableComponent
+    :headers="['posicao', 'equipe', 'vitórias', 'pontos']"
+    :table-style="$style.table"
+    :max-rows="10"
+    v-if="formulaStore.getTeams"
+  >
+    <ul
+      class="table-body"
+      v-for="(
+        { position, points, wins, Constructor }, index
+      ) in formulaStore.getTeams"
+      :key="index"
+      :style="{ gridTemplateColumns: $style.table }"
+    >
+      <li>{{ position }}</li>
+      <li>
+        <span>{{ Constructor.name }}</span>
+        <span class="nationality-team">{{ Constructor.nationality }}</span>
+      </li>
+      <li>{{ wins }}</li>
+      <li>{{ points }}</li>
+      <li>
+        <a :href="Constructor.url" target="_blank"
+          ><IconComponent :path="mdiOpenInNew" :size="17"></IconComponent
+        ></a>
+      </li>
+    </ul>
+  </TableComponent>
+  <p v-else>sem dados</p>
 </template>
 
 <script setup>
+import TableComponent from "../components/utilities/TableComponent.vue";
+import IconComponent from "../components/utilities/IconComponent.vue";
+import { mdiOpenInNew } from "@mdi/js";
+import { useFormulaStore } from "../stores/formulaStore";
 
+const formulaStore = useFormulaStore();
 </script>
 
-<style scoped>
+<style module>
+:export {
+  table: 1fr 1.3fr repeat(2, 1fr) 0.4fr;
+}
+</style>
 
+<style scoped>
+.nationality-team {
+  font-size: 0.6em;
+  margin-left: 4px;
+  font-weight: 500;
+  font-family: var(--font-regular);
+  opacity: 0.8;
+}
 </style>
